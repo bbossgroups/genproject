0af5f3ee977baf583351da6422e27639ccd29818
yinbp <yin-bp@163.com>
2017-05-15 23:21:16 +0800

    修改dubbo服务模板运行sh指令模板，以后台进程方式运行dubbo服务

--- resources/templates/dubbo/build/project-dubbo/runfiles/setup.sh
+++ resources/templates/dubbo/build/project-dubbo/runfiles/setup.sh
@@ -4,7 +4,7 @@ set "AGENT_VERSION=$AGENT_VERSION"
 set "AGENT_ID=$AGENT_ID"
 set "APPLICATION_NAME=$APPLICATION_NAME"
 set "AGENT_PATH=$AGENT_PATH"
-java -Xms1024m -Xmx1024m -Xmn512m -XX:PermSize=128M -XX:MaxPermSize=256M -javaagent:\\\$AGENT_PATH/pinpoint-bootstrap-1.6.0.jar -Dpinpoint.agentId=\\\$AGENT_ID -Dpinpoint.applicationName=\\\$APPLICATION_NAME -jar bboss-rt-${bboss_version}.jar 
+nohup java -Xms1024m -Xmx1024m -Xmn512m -XX:PermSize=128M -XX:MaxPermSize=256M -javaagent:\\\$AGENT_PATH/pinpoint-bootstrap-1.6.0.jar -Dpinpoint.agentId=\\\$AGENT_ID -Dpinpoint.applicationName=\\\$APPLICATION_NAME -jar bboss-rt-${bboss_version}.jar > dubbo.log &
 #else
-java -Xms1024m -Xmx1024m -Xmn512m -XX:PermSize=128M -XX:MaxPermSize=256M -jar bboss-rt-${bboss_version}.jar 
+nohup java -Xms1024m -Xmx1024m -Xmn512m -XX:PermSize=128M -XX:MaxPermSize=256M -jar bboss-rt-${bboss_version}.jar > dubbo.log & 
 #end
