89dcfcfea02f428332a75ad60960bff8fc6d659d
yinbp <yin-bp@163.com>
2017-05-15 23:50:01 +0800

    修改dubbo服务sh启动模板

--- resources/templates/dubbo/build/project-dubbo/runfiles/setup.sh
+++ resources/templates/dubbo/build/project-dubbo/runfiles/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #if($enablepinpoint.equals("true"))
-set "AGENT_VERSION=$AGENT_VERSION"
-set "AGENT_ID=$AGENT_ID"
-set "APPLICATION_NAME=$APPLICATION_NAME"
-set "AGENT_PATH=$AGENT_PATH"
+export AGENT_VERSION==$AGENT_VERSION
+export AGENT_ID=$AGENT_ID
+export APPLICATION_NAME=$APPLICATION_NAME
+export AGENT_PATH=$AGENT_PATH
 nohup java -Xms1024m -Xmx1024m -Xmn512m -XX:PermSize=128M -XX:MaxPermSize=256M -javaagent:\\\$AGENT_PATH/pinpoint-bootstrap-1.6.0.jar -Dpinpoint.agentId=\\\$AGENT_ID -Dpinpoint.applicationName=\\\$APPLICATION_NAME -jar bboss-rt-${bboss_version}.jar > dubbo.log &
 #else
 nohup java -Xms1024m -Xmx1024m -Xmn512m -XX:PermSize=128M -XX:MaxPermSize=256M -jar bboss-rt-${bboss_version}.jar > dubbo.log & 
