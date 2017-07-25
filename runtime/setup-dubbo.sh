08e530438e819016ecb900e99c93953c2d92b6aa
yinbp <yin-bp@163.com>
2017-07-25 23:03:07 +0800

    更新框架版本号

--- runtime/setup-dubbo.sh
+++ runtime/setup-dubbo.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-java -Xms1024m -Xmx1024m -Xmn512m -XX:PermSize=256M -XX:MaxPermSize=512M -jar bboss-rt-${bboss_version}.jar  --conf=config-dubbo.properties
+nohup java -Xms1024m -Xmx1024m -Xmn512m -XX:PermSize=256M -XX:MaxPermSize=512M -jar bboss-rt-${bboss_version}.jar  --conf=config-dubbo.properties > dubbo.log &
+tail -f dubbo.log
